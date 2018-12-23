0ae4c17ff2d1db5a4a0305f372126c90d4e0d179
jamesp <phillips.james@gmail.com>
2018-12-22 19:40:49 -0600

    cleaned up python code so it wasn't so crappy

--- connect-ipranges.py
+++ connect-ipranges.py
@@ -2,10 +2,19 @@
 import requests
 
 ip_ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()['prefixes']
+
+#Amazon Connect needs access to AMAZON_CONNECT, EC2, and CLOUDFRONT over port 443
 connect_ips = [item['ip_prefix'] for item in ip_ranges if (item["service"] == "AMAZON_CONNECT") and (item["region"] == "us-east-1")]
 ec2_ips = [item['ip_prefix'] for item in ip_ranges if (item["service"] == "EC2") and (item["region"] == "us-east-1")]
 cloudfront_ips = [item['ip_prefix'] for item in ip_ranges if (item["service"] == "CLOUDFRONT") and (item["region"] == "us-east-1")]
+connect_ranges = []
+
+for ip in connect_ips:
+    ranges.append(ip)
+for ip in ec2_ips:
+    ranges.append(ip)
+for ip in cloudfront_ips:
+    ranges.append(ip)
+
 print("ranges needing access to tcp/443:")
-for ip in connect_ips: print(str(ip))
-for ip in ec2_ips: print(str(ip))
-for ip in cloudfront_ips: print(str(ip))
+for ip in connect_ranges: print(str(ip))
